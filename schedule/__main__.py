94383d7790187a6933efd771c432fd9288fe5f7a
angr1it <andgr1it@gmail.com>
2023-06-06 14:23:52 +0000

    project structure

--- schedule/__main__.py
+++ schedule/__main__.py
@@ -1,5 +1,5 @@
-from scheduler import Scheduler
-from scheduler.tasks.sleeper import Sleeper
+from .scheduler import Scheduler
+from schedule.tasks.sleeper import Sleeper
 
 if __name__ == "__main__":
     job = Sleeper()
--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,19 @@
 import unittest
 
-from scheduler.job import Job
-from scheduler.tasks.sleeper import Sleeper
-from scheduler import Scheduler   
+from schedule.job import Job
+from schedule.tasks.sleeper import Sleeper
+from schedule.scheduler import Scheduler   
 
 class TestSleeper(unittest.TestCase):
     
     def test_Sleeper_is_Job(self):
         job = Sleeper()
         self.assertIsInstance(job, Job)
+
+    @unittest.expectedFailure
+    def test_Sleeper_is_Scheduler(self):
+        job = Sleeper()
+        self.assertIsInstance(job, Scheduler)
 
 if __name__ == '__main__':
     unittest.main()
